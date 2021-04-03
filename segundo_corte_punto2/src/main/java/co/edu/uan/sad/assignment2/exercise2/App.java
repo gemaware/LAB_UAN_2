31d583a1bf426a38b7159fc13530ebc8fa45ecd6
Geovani L Martinez <77243140+gemaware@users.noreply.github.com>
2021-04-02 19:41:19 -0500

    Update App.java

--- segundo_corte_punto2/src/main/java/co/edu/uan/sad/assignment2/exercise2/App.java
+++ segundo_corte_punto2/src/main/java/co/edu/uan/sad/assignment2/exercise2/App.java
@@ -8,6 +8,45 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Comparativa equals" );
     }
+}
+public class A {
+    int aField;
+    @Override
+    public boolean equals(Object o) {
+        if(this==o) {
+            return true;
+        }   
+        if(o==null) {
+            return false;
+        }
+        if(!(o instanceof A)) {
+            return false;
+        }
+        A other = (A) o;
+
+		return other.aField == afield; /* lineas adicionadas por geovani Martinez*/
+
+}
+
+}
+public class B extends A {
+    int bField;
+    @Override
+    public boolean equals(Object o) {
+        if(this==o) {
+            return true;
+        }
+        if(!super.equals(o)) {
+            return false;
+        }
+        if(!(o instanceof B)) {
+            return false;
+        }
+        B other = (B) o;
+
+        return super.equals(o) && bField.equals(other.bField);  /* lineas adicionadas por geovani Martinez*/
+
+
 }
